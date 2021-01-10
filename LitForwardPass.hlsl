38e312cc05821a8631539b1ca857775a13a1011f
James Gu <jamesgu521@gmail.com>
2021-01-10 15:23:09 -0600

    Initial commit :sparkle:

---
+++ LitForwardPass.hlsl
@@ -0,0 +1,184 @@
+#ifndef UNIVERSAL_FORWARD_LIT_PASS_INCLUDED
+#define UNIVERSAL_FORWARD_LIT_PASS_INCLUDED
+
+#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
+
+struct Attributes
+{
+    float4 positionOS   : POSITION;
+    float3 normalOS     : NORMAL;
+    float4 tangentOS    : TANGENT;
+    float2 texcoord     : TEXCOORD0;
+    float2 lightmapUV   : TEXCOORD1;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+};
+
+struct Varyings
+{
+    float2 uv                       : TEXCOORD0;
+    DECLARE_LIGHTMAP_OR_SH(lightmapUV, vertexSH, 1);
+
+#if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+    float3 positionWS               : TEXCOORD2;
+#endif
+
+#ifdef _NORMALMAP
+    float4 normalWS                 : TEXCOORD3;    // xyz: normal, w: viewDir.x
+    float4 tangentWS                : TEXCOORD4;    // xyz: tangent, w: viewDir.y
+    float4 bitangentWS              : TEXCOORD5;    // xyz: bitangent, w: viewDir.z
+#else
+    float3 normalWS                 : TEXCOORD3;
+    float3 viewDirWS                : TEXCOORD4;
+#endif
+
+    half4 fogFactorAndVertexLight   : TEXCOORD6; // x: fogFactor, yzw: vertex light
+
+#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+    float4 shadowCoord              : TEXCOORD7;
+#endif
+
+    float4 positionCS               : SV_POSITION;
+    UNITY_VERTEX_INPUT_INSTANCE_ID
+    UNITY_VERTEX_OUTPUT_STEREO
+};
+
+void InitializeInputData(Varyings input, half3 normalTS, out InputData inputData)
+{
+    inputData = (InputData)0;
+
+#if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+    inputData.positionWS = input.positionWS;
+#endif
+
+#ifdef _NORMALMAP
+    half3 viewDirWS = half3(input.normalWS.w, input.tangentWS.w, input.bitangentWS.w);
+    inputData.normalWS = TransformTangentToWorld(normalTS,
+        half3x3(input.tangentWS.xyz, input.bitangentWS.xyz, input.normalWS.xyz));
+#else
+    half3 viewDirWS = input.viewDirWS;
+    inputData.normalWS = input.normalWS;
+#endif
+
+    inputData.normalWS = NormalizeNormalPerPixel(inputData.normalWS);
+    viewDirWS = SafeNormalize(viewDirWS);
+    inputData.viewDirectionWS = viewDirWS;
+
+#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+    inputData.shadowCoord = input.shadowCoord;
+#elif defined(MAIN_LIGHT_CALCULATE_SHADOWS)
+    inputData.shadowCoord = TransformWorldToShadowCoord(inputData.positionWS);
+#else
+    inputData.shadowCoord = float4(0, 0, 0, 0);
+#endif
+
+    inputData.fogCoord = input.fogFactorAndVertexLight.x;
+    inputData.vertexLighting = input.fogFactorAndVertexLight.yzw;
+    inputData.bakedGI = SAMPLE_GI(input.lightmapUV, input.vertexSH, inputData.normalWS);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                  Vertex and Fragment functions                            //
+///////////////////////////////////////////////////////////////////////////////
+
+float RayPlaneIntersection( float3 rayDir, float3 rayPos, float3 planeNormal, float3 planePos)
+{
+    float denom = dot(planeNormal, rayDir);
+    denom = max(denom, 0.000001); // avoid divide by zero
+    float3 diff = planePos - rayPos;
+    return dot(diff, planeNormal) / denom;
+}
+float BillboardVerticalZDepthVert(Attributes IN, inout Varyings OUT)
+{
+    // billboard mesh towards camera
+    float3 vpos = mul((float3x3)unity_ObjectToWorld, IN.positionOS.xyz);
+    float4 worldCoord = float4(unity_ObjectToWorld._m03, unity_ObjectToWorld._m13, unity_ObjectToWorld._m23, 1);
+    float4 viewPos = mul(UNITY_MATRIX_V, worldCoord) + float4(vpos, 0);
+    
+    // view to clip space
+    OUT.positionCS = mul(UNITY_MATRIX_P, viewPos);
+
+    // calculate distance to vertical billboard plane seen at this vertex's screen position
+    float3 planeNormal = normalize(float3(UNITY_MATRIX_V._m20, 0.0, UNITY_MATRIX_V._m22));
+    float3 planePoint = unity_ObjectToWorld._m03_m13_m23;
+    float3 rayStart = _WorldSpaceCameraPos.xyz;
+    float3 rayDir = -normalize(mul(UNITY_MATRIX_I_V, float4(viewPos.xyz, 1.0)).xyz - rayStart); // convert view to world, minus camera pos
+    float dist = RayPlaneIntersection(rayDir, rayStart, planeNormal, planePoint);
+
+    // calculate the clip space z for vertical plane
+    float4 planeOutPos = mul(UNITY_MATRIX_VP, float4(rayStart + rayDir * dist, 1.0));
+    float newPosZ = planeOutPos.z / planeOutPos.w * OUT.positionCS.w;
+    
+    // use the closest clip space z
+    #if defined(UNITY_REVERSED_Z)
+    newPosZ = max(OUT.positionCS.z, newPosZ);
+    #else
+    newPosZ = min(OUT.positionCS.z, newPosZ);
+    #endif
+
+    return newPosZ;
+}
+
+// Used in Standard (Physically Based) shader
+Varyings LitPassVertex(Attributes input)
+{
+    Varyings output = (Varyings)0;
+
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_TRANSFER_INSTANCE_ID(input, output);
+    UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+
+    VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS.xyz);
+    VertexNormalInputs normalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
+    half3 viewDirWS = GetCameraPositionWS() - vertexInput.positionWS;
+    half3 vertexLight = VertexLighting(vertexInput.positionWS, normalInput.normalWS);
+    half fogFactor = ComputeFogFactor(vertexInput.positionCS.z);
+
+    output.uv = TRANSFORM_TEX(input.texcoord, _MainTex);
+
+#ifdef _NORMALMAP
+    output.normalWS = half4(normalInput.normalWS, viewDirWS.x);
+    output.tangentWS = half4(normalInput.tangentWS, viewDirWS.y);
+    output.bitangentWS = half4(normalInput.bitangentWS, viewDirWS.z);
+#else
+    output.normalWS = NormalizeNormalPerVertex(normalInput.normalWS);
+    output.viewDirWS = viewDirWS;
+#endif
+
+    OUTPUT_LIGHTMAP_UV(input.lightmapUV, unity_LightmapST, output.lightmapUV);
+    OUTPUT_SH(output.normalWS.xyz, output.vertexSH);
+
+    output.fogFactorAndVertexLight = half4(fogFactor, vertexLight);
+
+#if defined(REQUIRES_WORLD_SPACE_POS_INTERPOLATOR)
+    output.positionWS = vertexInput.positionWS;
+#endif
+
+#if defined(REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR)
+    output.shadowCoord = GetShadowCoord(vertexInput);
+#endif
+
+    output.positionCS = vertexInput.positionCS;
+    output.positionCS.z = BillboardVerticalZDepthVert(input, output);
+
+    return output;
+}
+
+// Used in Standard (Physically Based) shader
+half4 LitPassFragment(Varyings input) : SV_Target
+{
+    UNITY_SETUP_INSTANCE_ID(input);
+    UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+
+    SurfaceData surfaceData;
+    InitializeStandardLitSurfaceData(input.uv, surfaceData);
+
+    InputData inputData;
+    InitializeInputData(input, surfaceData.normalTS, inputData);
+
+    half4 color = UniversalFragmentPBR(inputData, surfaceData.albedo, surfaceData.metallic, surfaceData.specular, surfaceData.smoothness, surfaceData.occlusion, surfaceData.emission, surfaceData.alpha);
+
+    color.rgb = MixFog(color.rgb, inputData.fogCoord);
+    return color;
+}
+
+#endif
